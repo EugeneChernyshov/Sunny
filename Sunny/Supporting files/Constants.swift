b29bbc4587e7c270e50163a3060c9619acb05819
Evheniy Chernyshov <kh.410ever@gmail.com>
2020-11-23 18:49:47 +0200

    General commit

---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Evgeniy Chernyshov on 25/09/2020.
+//  Copyright © 2020 Evgeniy Chernyshov. All rights reserved.
+//  Swift 5
+
+import Foundation
+
+let apiKey = "358389cc2e7b7f987ac85f1075b911c6"
